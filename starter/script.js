1d65126c9451492b3fff220d2463dd510f2ad4da
Saodat <saodat.riskiyeva@gmail.com>
2024-05-29 17:10:22 -0400

    functions

--- starter/script.js
+++ starter/script.js
@@ -23,3 +23,54 @@ createBooking('LH123');
 createBooking('LH123', 2, 800);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1050);
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas',
+  passport: 23457896,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 23457896) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+// flightNum = flight;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+  console.log(person.passport);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const UpperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', UpperFirstWord);
+
+transformer('JavaScript is the best!', oneWord);
