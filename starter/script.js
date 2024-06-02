1a3219000ae93950a300181c9d4e922191f6d7e9
Saodat <saodat.riskiyeva@gmail.com>
2024-06-01 20:26:00 -0400

    closure

--- starter/script.js
+++ starter/script.js
@@ -183,22 +183,77 @@
 // console.log(addVAT2(100));
 // console.log(addVAT2(23));
 
-const runOnce = function () {
-  console.log('This will never run again!');
-};
-runOnce();
+// const runOnce = function () {
+//   console.log('This will never run again!');
+// };
+// runOnce();
 
-// Immediately invoked function expression
-(function () {
-  console.log('This will never run again 2!');
-})();
+// // Immediately invoked function expression
+// (function () {
+//   console.log('This will never run again 2!');
+// })();
 
-(() => console.log('This will never run again 3!'))();
+// (() => console.log('This will never run again 3!'))();
 
-{
-  const isPrivate = 23;
-  var notPrivate = 46;
-}
+// {
+//   const isPrivate = 23;
+//   var notPrivate = 46;
+// }
 
-// console.log(isPrivate);
-console.log(notPrivate);
+// // console.log(isPrivate);
+// console.log(notPrivate);
+
+// const secureBooking = function () {
+//   let passengerCount = 0;
+
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+// let f;
+// const g = function () {
+//   const a = 50;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 7;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f();
+// console.dir(f);
+
+// h();
+// f();
+// console.dir(f);
+
+// const boardPassengers = function (n, wait) {
+//   const perGroup = n / 3;
+
+//   setTimeout(function () {
+//     console.log(`We are now boarding all ${n} passengers`);
+//     console.log(`There are 3 groups, each with ${perGroup} passengers`);
+//   }, wait * 1000);
+
+//   console.log(`Will start boarding in ${wait} seconds`);
+// };
+
+// boardPassengers(180, 3);
+// // setTimeout(function () {
+// //   console.log('TIMER');
+// // }, 1000);
